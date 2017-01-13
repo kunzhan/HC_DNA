793a6fcd9edd9220da4a4f9f38eb1a59f6717bc4
绽琨 <kunzhan@users.noreply.github.com>
2017-01-13 21:24:19 +0800

    Update figure_03.m

--- figure_03.m
+++ figure_03.m
@@ -4,7 +4,7 @@ load data_in_mem
 figure(1)
 for t = 1:3
     k = hyper_chaotic_system(numel(data{4,t})); % note: Change x1(1,1) to 0.12+1e-15;
-    subplot(2,3,t),imshow(data{4,t})
-    subplot(2,3,t+3),imshow(uint8(inverse_hyper_chaotic_DNA(data{1,t},k,'A')));
+    subplot(2,3,t),imshow(data{2,t})
+    subplot(2,3,t+3),imshow(uint8(inverse_hyper_chaotic_DNA(data{3,t},k,'A')));
 end
     
