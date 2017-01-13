df4bf0567772c587469d47fa9c0cecf78b064b30
绽琨 <kunzhan@users.noreply.github.com>
2017-01-13 21:32:02 +0800

    Update corr_horizontal.m

--- tools/corr_horizontal.m
+++ tools/corr_horizontal.m
@@ -6,17 +6,13 @@ for i=1:m
         C=[C A(i,j)];
     end
 end
-% 将m*n矩阵转换为一维向量，逐行元素进行，逐个添加到一维向量C当中
-% 图像水平方向上的像素相连在一起
 
 for i=1:m*n
     B(i)=double(C(i));
 end
-% 将每个像素值转换为实数值
 
 m1=mean(B(1:m*n-1));
 m2=mean(B(2:m*n));
-% 求平均值
 
 t=sym(0);
 k1=double(t);
