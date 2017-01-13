6b1c42e4687d370e193f967029a1c8e5f95fad10
绽琨 <kunzhan@users.noreply.github.com>
2017-01-13 21:27:40 +0800

    Update figure_04.m

--- figure_04.m
+++ figure_04.m
@@ -5,8 +5,8 @@ k = 0;
 figure(1),
 for i = 1:6
     k = 4*(i-1) + 1;
-    subplot(6,4,k),imshow(data{4,i})
-    subplot(6,4,k+1),imhist(data{4,i})
-    subplot(6,4,k+2),imshow(data{1,i},[])
-	subplot(6,4,k+3),imhist(uint8(data{1,i}))
+    subplot(6,4,k),imshow(data{2,i})
+    subplot(6,4,k+1),imhist(data{2,i})
+    subplot(6,4,k+2),imshow(data{4,i},[])
+	subplot(6,4,k+3),imhist(uint8(data{4,i}))
 end
